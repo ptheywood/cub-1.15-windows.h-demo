be559f11d178f4c404a3d68bdf7558bfce6fbb47
Peter Heywood <peethwd@gmail.com>
2022-01-19 13:33:00 +0000

    Test a potential jitify patch for Windows.h with CUB 1.15 support.
    This undefs small while cub is parsed, then re-defs it afterwards, as a short term fix

--- src/windows-before-cub.cu
+++ src/windows-before-cub.cu
@@ -1,8 +1,13 @@
 // Including windows.h before cub/cub.cuh for CUB 1.5.0 results in a compile error.
 #if defined(_WIN32)
     #include <Windows.h>
+    #pragma push_macro("small")
+    #undef small
 #endif
 #include <cub/cub.cuh>
+#if defined(_WIN32)
+    #pragma pop_macro("small")
+#endif
 int main(int argc, char* argv[]) {
     return EXIT_SUCCESS;
 }
